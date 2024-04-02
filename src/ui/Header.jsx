8c8d29d4889f4993eee99da425441a04605c6031
Moh Aadil <aadilrehman.development@gmail.com>
2024-04-02 17:09:03 +0530

    Few changes

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,7 +5,9 @@ import Username from "../features/user/Username";
 function Header() {
 	return (
 		<header className="bg-yellow-400 uppercase px-4 py-3 border-b border-stone-200 sm:px-6 flex items-center justify-between">
-			<Link to="/" className="tracking-widest"> Fast React Pizz Co.</Link>
+			<Link to="/" className="tracking-widest font-semibold text-xl">
+				Pizza Paradise
+			</Link>
 			<SearchOrder />
 
 			<Username />
